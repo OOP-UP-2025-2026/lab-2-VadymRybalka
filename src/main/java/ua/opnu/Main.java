8501a8683aca4c197312e3e0089edc1c35664667
Vadym Rybalka <4833085@stud.op.edu.ua>
2025-09-20 18:59:17 +0300

    Rybalka Vadym, lab2

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,5 +2,18 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
+
+        Student vadym = new Student("Вадім Рибалка", 2);
+
+        vadym.addCourse("Теорія Алгоритмів");
+        vadym.addCourse("Засоби ШІ у веб-технологіях");
+        vadym.addCourse("ООП");
+        vadym.addCourse("Укр. мова");
+        vadym.addCourse("Організація баз даних та знань");
+        vadym.addCourse("Теорія ймовірностей й мат.статистика");
+
+        System.out.println(vadym.getName() + ": дисциплін - " + vadym.getCourseCount());
+        System.out.println(vadym.getName() + ": рік - " + vadym.getYear());
+        System.out.println(vadym.getName() + ": навчання - " + vadym.getTuition());
     }
 }
